4a535a6d9aa631e039bee39f26075c740bd94d7b
NorthBei <seekingfor520@gmail.com>
2018-05-16 21:15:44 +0800

    build scroll-select.js for es5 to include by script tag
    1.move orginal example files to example/es6
    2.modify orginal webpack config (just add /es6 path)
    3.add webpack.config.es5.js to build this proj to a es5 lib
    4.add 2 npm script in package.json for webpack to build
    5. add some project description  in ReadMe file

---
+++ example/es5/index.js
@@ -0,0 +1,37 @@
+var el = document.getElementById('select')
+
+var select = new Select(el, {
+  data: getData(1980)
+})
+select.value(1990)
+
+select.on('change', function (v) {
+  console.log(v)
+})
+
+function getData(from) {
+  var data = []
+  for (var i = 0; i < 15; i ++) {
+    data.push({
+      id: String(from + i),
+      text: String(from + i) + '年'
+    })
+  }
+  return data
+}
+
+document.getElementById('prev').addEventListener('click', function () {
+  select.prev()
+}, false)
+
+document.getElementById('next').addEventListener('click', function () {
+  select.next()
+}, false)
+
+document.getElementById('rebuild').addEventListener('click', function () {
+  select.setData(getData(1970))
+}, false)
+
+document.getElementById('unbind').addEventListener('click', function () {
+  select.unbind()
+}, false)
