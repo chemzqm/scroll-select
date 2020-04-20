07a3aa4ae1b6881bdcbaf00561fbd779b8f28189
Nathan Cheval <nathan.cheval@outlook.fr>
2020-04-20 09:44:27 +0200

    Modify to use as a real select

--- example/es5/index.js
+++ example/es5/index.js
@@ -1,37 +1,59 @@
-var el = document.getElementById('select')
+$(document).ready(function () {
+    $(".scrollable-select").each(function () {
+        let selectId = $(this).attr('select-id')
+        let optionsList = $(this).find('option')
+        let defaultOption = $(this).find('option[selected]').val()
 
-var select = new Select(el, {
-  data: getData(1980)
-})
-select.value(1990)
+        $(this).after('<div id="' + selectId + '" class="scrollable_select_div"></div>')
+        $('#' + selectId).append(
+                '<div class="select_arrow">' +
+                    '<button id="rebuild">rebuild</button>' + 
+                    '<button id="prev">prev</button>' + 
+                    '<button id="next">next</button>' +
+                    '<button id="unbind">unbind</button>' +
+                '</div>')
 
-select.on('change', function (v) {
-  console.log(v)
-})
+        let el = document.getElementById(selectId)
+        let select = new Select(el, {
+            data: getData(optionsList)
+        })
 
-function getData(from) {
-  var data = []
-  for (var i = 0; i < 15; i ++) {
-    data.push({
-      id: String(from + i),
-      text: String(from + i) + '年'
-    })
-  }
-  return data
-}
+        function getData(optionsList) {
+            let data = []
+            for (let i = 0; i < optionsList.length; i++) {
+                let optionText = optionsList[i].text
+                let optionValue = optionsList[i].value
+                data.push({
+                    id: optionValue,
+                    text: optionText
+                })
+            }
+            return data
+        }
 
-document.getElementById('prev').addEventListener('click', function () {
-  select.prev()
-}, false)
+        select.value(defaultOption)
+        select.on('change', function (newValue) {
+            let select = $(this).attr('select_id');
+            $(".scrollable-select[select-id='" + select + "']").val(newValue).change()
+            $(this).find('option[value=' + newValue + ']').prop('selected', true)
+        })
 
-document.getElementById('next').addEventListener('click', function () {
-  select.next()
-}, false)
+        document.getElementById('prev_' + selectId).addEventListener('click', function () {
+          select.prev()
+        }, false)
 
-document.getElementById('rebuild').addEventListener('click', function () {
-  select.setData(getData(1970))
-}, false)
+        document.getElementById('next_' + selectId).addEventListener('click', function () {
+          select.next()
+        }, false)
+      
+        document.getElementById('rebuild_' + selectId).addEventListener('click', function () {
+          select.setData(getData(optionsList))
+        }, false)
 
-document.getElementById('unbind').addEventListener('click', function () {
-  select.unbind()
-}, false)
+        document.getElementById('unbind_' + selectId).addEventListener('click', function () {
+          select.unbind()
+        }, false)
+
+    });
+});
+
